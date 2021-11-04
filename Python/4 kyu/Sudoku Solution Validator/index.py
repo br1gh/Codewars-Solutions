880e9c8f4e444b7382c683f0f36f35b255d703ab
brigh <43861280+br1gh@users.noreply.github.com>
2021-11-04 19:18:37 +0100

    Create index.py

---
+++ Python/4 kyu/Sudoku Solution Validator/index.py
@@ -0,0 +1,4 @@
+#https://www.codewars.com/kata/604a1de6d117a400159239e5
+def validSolution(arr):
+    x = [arr[j][i:i+3]+arr[j+1][i:i+3]+arr[j+2][i:i+3] for i in range(0,9,3) for j in range(0,9,3)]
+    return [sorted([i[j] for i in arr]) for j in range(9)] == [sorted(i) for i in x]
