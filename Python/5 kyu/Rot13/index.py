e2d579ec87e51a2200ab7b94ba5a827ee7c515cf
brigh <43861280+br1gh@users.noreply.github.com>
2021-11-04 23:42:06 +0100

    Update index.py

--- Python/5 kyu/Rot13/index.py
+++ Python/5 kyu/Rot13/index.py
@@ -1,5 +1,10 @@
 #codewars.com/kata/530e15517bc88ac656000716/python
+import string
 def rot13(message):
+    return message.encode("rot13")
+
+#Alternative version
+def rot131(message):
     a = 'nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM'
     b = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
     return ''.join([b[a.index(i)] if i in a else i for i in message])
