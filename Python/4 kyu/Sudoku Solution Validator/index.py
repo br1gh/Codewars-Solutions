f8b0d33d48151a87304af5355bfd1ea143cc75aa
brigh <43861280+br1gh@users.noreply.github.com>
2021-11-04 19:27:58 +0100

    Update index.py

--- Python/4 kyu/Sudoku Solution Validator/index.py
+++ Python/4 kyu/Sudoku Solution Validator/index.py
@@ -1,4 +1,4 @@
-#https://www.codewars.com/kata/529bf0e9bdf7657179000008
+#codewars.com/kata/529bf0e9bdf7657179000008
 def validSolution(arr):
     x = [arr[j][i:i+3]+arr[j+1][i:i+3]+arr[j+2][i:i+3] for i in range(0,9,3) for j in range(0,9,3)]
     return [sorted([i[j] for i in arr]) for j in range(9)] == [sorted(i) for i in x]
