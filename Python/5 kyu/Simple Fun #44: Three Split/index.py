71145dc965884a78395822688970f6ae7b5fdf51
brigh <43861280+br1gh@users.noreply.github.com>
2021-11-09 23:20:25 +0100

    Delete Python/5 kyu/Simple Fun #44: Three Split directory

--- Python/5 kyu/Simple Fun #44: Three Split/index.py
+++
@@ -1,8 +0,0 @@
-#codewars.com/kata/588833be1418face580000d8/python
-def three_split(a):
-    count = 0
-    for i in range(1,len(a)-1):  
-        if 2*sum(a[:i]) == sum(a[i:]):
-            for j in range(1,len(a)-i): 
-                if sum(a[i:i+j]) == sum(a[i+j:]): count += 1
-    return count
